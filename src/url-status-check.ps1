f2b6a7a77ae8d34d1424ca9fb35735197fb18190
Danielle Adams <danielle.adams3@extendhealth.com>
2022-10-17 14:17:53 -0600

    ARCH-1615 - +semver:minor Update syntax for setting outputs

--- src/url-status-check.ps1
+++ src/url-status-check.ps1
@@ -10,8 +10,8 @@ try {
     Write-Output "Status Code: $($response.StatusCode)"
     Write-Output "Content: $($response.Content)"
 
-    echo "::set-output name=status_code::$($response.StatusCode)"
-    echo "::set-output name=content::$($response.Content)"
+    echo "status_code=$($response.StatusCode)" >> $GITHUB_OUTPUT
+    echo "content=$($response.Content)" >> $GITHUB_OUTPUT
 
     if ($errorOnBadStatus -and $response.StatusCode -ge 400) {
         throw "The status code $($response.StatusCode) indicates either a problem with the request or the application running at $url."
