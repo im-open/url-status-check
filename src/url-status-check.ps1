b977176dcfe5f8db49cfc5292f486e38c799c87f
Danielle Adams <danielle.adams3@extendhealth.com>
2022-10-25 15:21:48 -0600

    ARCH-1615 - Updating syntax for powershell

--- src/url-status-check.ps1
+++ src/url-status-check.ps1
@@ -10,8 +10,8 @@ try {
     Write-Output "Status Code: $($response.StatusCode)"
     Write-Output "Content: $($response.Content)"
 
-    echo "status_code=$($response.StatusCode)" >> $GITHUB_OUTPUT
-    echo "content=$($response.Content)" >> $GITHUB_OUTPUT
+    "status_code=$($response.StatusCode)" >> $env:GITHUB_OUTPUT
+    "content=$($response.Content)" >> $env:GITHUB_OUTPUT
 
     if ($errorOnBadStatus -and $response.StatusCode -ge 400) {
         throw "The status code $($response.StatusCode) indicates either a problem with the request or the application running at $url."
